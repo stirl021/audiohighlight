8af8e89801130f750209f9d3dcd7b65fb79447f0
stirl021 <stirl021@gmail.com>
2017-11-21 15:47:32 +0000

    console.log output

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ audio.addEventListener('loadeddata', clueThroughEvent, false);
 function clueThroughEvent(e) {
 	
 	var text = e.target.textTracks[0].activeCues[0].text;
-	console.log(text);
+
+	console.log('clue through event: ' +text); // Peppa -> OK
 }
 
 function clueWithoutEvent() {
+
 	var audio = document.getElementById('audiofile');
 	var text = audio.textTracks[0].activeCues[0].text;
-	console.log(text);
+
+	console.log('clue without event: ' +text);  //Uncaught TypeError: Cannot read property '0' of null
 }
 
 clueWithoutEvent();
