1c7b33b1034c650ba29266d651a82f5931c0744e
stirl021 <stirl021@gmail.com>
2017-11-21 15:41:48 +0000

    Clues through event and without event.

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-var audioFile = document.getElementById('audiofile');
-audiofile.textTracks[0].mode = "hidden";
+var audio = document.getElementById('audiofile');
 
-var trackElement = document.getElementById('track');
+audio.addEventListener('loadeddata', clueThroughEvent, false);
 
-audioFile.addEventListener('timeupdate', showTime, false);
-
-function showTime(e) {
+function clueThroughEvent(e) {
 	
-	console.log(e);
 	var text = e.target.textTracks[0].activeCues[0].text;
-	//var text = e.target.textTracks[0].cues[1].text
-	trackElement.textContent = text;
+	console.log(text);
 }
 
+function clueWithoutEvent() {
+	var audio = document.getElementById('audiofile');
+	var text = audio.textTracks[0].activeCues[0].text;
+	console.log(text);
+}
 
-audioFile.addEventListener('loadeddata', joinCues, false);
-
-/*function getTrack(e) {
-		
-	console.log(e);
-	cues = e.target.textTracks[0].cues.length;
-	return cues;
-}*/
-
-function joinCues(e) {
-	
-	var cues = e.target.textTracks[0].cues
-	
-	console.log(typeof(cues));
-  }
+clueWithoutEvent();
 
